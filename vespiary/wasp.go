033aa1c86f20dc7696a65aaf48e95f623fa5a56c
Julien BONACHERA <julien.bonachera@fortuneo.com>
2020-07-11 16:53:25 +0200

    remove legacy auth support

--- vespiary/wasp.go
+++ vespiary/wasp.go
@@ -38,24 +38,10 @@ func (s *WaspAuthenticationServer) Serve(grpcServer *grpc.Server) {
 	auth.RegisterAuthenticationServer(grpcServer, s)
 }
 
-func (s *WaspAuthenticationServer) legacyAuth(ctx context.Context, input *auth.WaspAuthenticationRequest) (*auth.WaspAuthenticationResponse, error) {
-	device, err := s.state.DeviceByName(string(input.MQTT.Username), string(input.MQTT.ClientID))
-	if err != nil {
-		s.fsm.CreateDevice(ctx, string(input.MQTT.Username), string(input.MQTT.ClientID), fingerprintBytes(input.MQTT.Password), false)
-		return nil, status.Error(codes.InvalidArgument, "invalid username or password")
-	}
-	if device.Active && device.Password == fingerprintBytes(input.MQTT.Password) {
-		return &auth.WaspAuthenticationResponse{
-			ID:         device.ID,
-			MountPoint: device.Owner,
-		}, nil
-	}
-	return nil, status.Error(codes.InvalidArgument, "invalid username or password")
-}
 func (s *WaspAuthenticationServer) AuthenticateMQTTClient(ctx context.Context, input *auth.WaspAuthenticationRequest) (*auth.WaspAuthenticationResponse, error) {
 	account, err := s.state.AccountByDeviceUsername(string(input.MQTT.Username))
 	if err != nil {
-		return s.legacyAuth(ctx, input)
+		return nil, status.Error(codes.InvalidArgument, "invalid username or password")
 	}
 	device, err := s.state.DeviceByName(account.ID, string(input.MQTT.ClientID))
 	if err != nil {
