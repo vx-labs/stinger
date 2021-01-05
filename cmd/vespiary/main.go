09cbfb44a81a977ca1de6d58fb20fe74efc406a6
Julien BONACHERA <julien.bonachera@fortuneo.com>
2021-01-05 19:18:12 +0100

    support ipv6 local address

--- cmd/vespiary/main.go
+++ cmd/vespiary/main.go
@@ -34,30 +34,13 @@ import (
 )
 
 func localPrivateHost() string {
-	ifaces, err := net.Interfaces()
+	conn, err := net.Dial("udp", "example.net:80")
 	if err != nil {
 		panic(err)
 	}
-
-	for _, v := range ifaces {
-		if v.Flags&net.FlagLoopback != net.FlagLoopback && v.Flags&net.FlagUp == net.FlagUp {
-			h := v.HardwareAddr.String()
-			if len(h) == 0 {
-				continue
-			} else {
-				addresses, _ := v.Addrs()
-				if len(addresses) > 0 {
-					ip := addresses[0]
-					if ipnet, ok := ip.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-						if ipnet.IP.To4() != nil {
-							return ipnet.IP.String()
-						}
-					}
-				}
-			}
-		}
-	}
-	panic("could not find a valid network interface")
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP.String()
 }
 func findPeers(name, tag string, minimumCount int) ([]string, error) {
 	config := consulapi.DefaultConfig()
